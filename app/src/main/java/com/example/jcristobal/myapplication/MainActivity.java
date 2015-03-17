e32c11045013e62e591c93af023e4e8081716ba5
J. Cristóbal López <tobas92@gmail.com>
2015-03-17 19:08:17 +0100

    Primera aproximación con Android Studio

---
+++ app/src/main/java/com/example/jcristobal/myapplication/MainActivity.java
@@ -0,0 +1,67 @@
+package com.example.jcristobal.myapplication;
+
+import android.support.v7.app.ActionBarActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.view.View.OnClickListener;
+import android.content.Intent;
+
+
+public class MainActivity extends ActionBarActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        //Obtenemos una referencia a los controles de la interfaz
+        final Button boton = (Button)findViewById(R.id.button);
+
+        //Implementamos el evento “click” del botón
+        boton.setOnClickListener(new OnClickListener() {
+            @Override
+            public void onClick(View v) {   //http://www.sgoliver.net/blog/desarrollando-una-aplicacion-android-sencilla/
+                Intent intent =
+                        new Intent(MainActivity.this, saludo.class);
+
+                startActivity(intent);
+            }
+        });
+
+
+
+    }
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+
+
+    //public void selfDestruct(View view) {
+        // Kabloey
+    //}
+
+    }
---
+++ app/src/main/java/com/example/jcristobal/myapplication/saludo.java
@@ -0,0 +1,33 @@
+package com.example.jcristobal.myapplication;
+
+/**
+ * Created by JCristobal on 17/03/2015.
+ */
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class saludo extends Activity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.saludo_main);
+
+        //Obtenemos una referencia a los controles de la interfaz
+        final Button boton2 = (Button)findViewById(R.id.button2);
+
+        //Implementamos el evento “click” del botón
+        boton2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent2 =
+                        new Intent(saludo.this, MainActivity.class);
+
+                startActivity(intent2);
+            }
+        });
+    }
+}
