cda029a340f950dd265e60b4b1ecb9e6cde2f8f9
J. Cristóbal López <tobas92@gmail.com>
2015-05-03 12:18:02 +0200

    Añado un widget básico a la aplicación

---
+++ app/src/main/java/com/example/jcristobal/myapplication/MiWidget.java
@@ -0,0 +1,18 @@
+package com.example.jcristobal.myapplication;
+
+/**
+ * Created by JCristobal on 29/04/2015.
+ */
+import android.appwidget.AppWidgetManager;
+import android.appwidget.AppWidgetProvider;
+import android.content.Context;
+
+public class MiWidget extends AppWidgetProvider {
+    @Override
+    public void onUpdate(Context context,
+                         AppWidgetManager appWidgetManager,
+                         int[] appWidgetIds) {
+        //Actualizar el widget
+        //...
+    }
+}
